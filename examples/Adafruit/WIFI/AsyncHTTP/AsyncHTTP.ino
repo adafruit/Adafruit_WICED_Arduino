48476353335bd40053424422b8f0fff152426653
huynguyen10 <quanghuy.cse@gmail.com>
2015-11-18 08:52:28 +0100

    CRC checker

--- examples/Adafruit/WIFI/AsyncHTTP/AsyncHTTP.ino
+++ examples/Adafruit/WIFI/AsyncHTTP/AsyncHTTP.ino
@@ -25,7 +25,35 @@
 #define CONTENT              ""
 #define METHOD               GET_METHOD
 
+
 int wifi_error = -1; // FAIL
+
+static PROGMEM prog_uint32_t crc_table[16] = {
+  0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
+  0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
+  0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
+  0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c
+};
+
+unsigned long crc_update(unsigned long crc, byte data)
+{
+  byte tbl_idx;
+  tbl_idx = crc ^ (data >> (0 * 4));
+  crc = pgm_read_dword_near(crc_table + (tbl_idx & 0x0f)) ^ (crc >> 4);
+  tbl_idx = crc ^ (data >> (1 * 4));
+  crc = pgm_read_dword_near(crc_table + (tbl_idx & 0x0f)) ^ (crc >> 4);
+  return crc;
+}
+
+unsigned long crc_string(char *s)
+{
+  unsigned long crc = ~0L;
+  while (*s)
+    crc = crc_update(crc, *s++);
+  crc = ~crc;
+  return crc;
+}
+
 /**************************************************************************/
 /*!
     @brief  Connect to pre-specified AP
@@ -64,6 +92,12 @@ void rxCallback(uint8_t* data, uint16_t data_length, uint16_t available)
 {
   Serial.print(F("Data Received!\r\n"));
   Serial.println((char*)data);
+
+  char* p_data = strstr((char*)data, "\r\n\r\n");
+  p_data += 4;
+  unsigned long crc = crc_string(p_data);
+  Serial.print(F("\r\nCRC = "));
+  Serial.println(crc, HEX);
 }
 
 /**************************************************************************/
---
+++ examples/Adafruit/WIFI/AsyncHTTP/http_server/crc32.py
@@ -0,0 +1,10 @@
+import sys
+import binascii
+
+def CRC32_from_file(filename):
+    buf = open(filename,'rb').read()
+    buf = (binascii.crc32(buf) & 0xFFFFFFFF)
+    return "%08X" % buf
+        
+for eachFile in sys.argv[1:]:
+    print CRC32_from_file(eachFile)
