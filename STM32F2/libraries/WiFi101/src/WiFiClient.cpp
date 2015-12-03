279e7ff61b7376b6f8ac74f377cb58dcd58bb318
hathach <thach@tinyusb.org>
2015-12-03 15:10:25 +0700

    change CFG_ARDUINO_CODE_MAGIC from Ladides to Decoded

--- STM32F2/libraries/WiFi101/src/WiFiClient.cpp
+++ STM32F2/libraries/WiFi101/src/WiFiClient.cpp
@@ -144,10 +144,7 @@ size_t WiFiClient::write(const uint8_t *buf, size_t size)
   VERIFY(ERROR_NONE == FEATHERLIB->sdep_execute_extend(SDEP_CMD_TCP_WRITE, 4, &_tcp_handle, size, buf, NULL, NULL), 0);
 
   // if packet is not buffered --> send out immediately
-  if (!_packet_buffering)
-  {
-    this->flush();
-  }
+  if (!_packet_buffering) this->flush();
 
   return size;
 }
