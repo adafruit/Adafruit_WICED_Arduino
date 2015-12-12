d56be077673c703a5284faa07fd482dc59d4e213
hathach <thach@tinyusb.org>
2015-12-13 01:28:48 +0700

    add example sketch for saveProfile()

---
+++ examples/Adafruit/WIFI/Client/WifiProfileSaveConnected/WifiProfileSaveConnected.ino
@@ -0,0 +1,131 @@
+/*
+
+ */
+#include <WiFi101.h>
+
+char ssid[]   = "yourNetwork";     //  your network SSID (name)
+char pass[]   = "secretPassword";  // your network password
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(1); // wait for serial port to connect. Needed for native USB port only
+  }
+
+  if ( WiFi.checkProfile(ssid) )
+  {
+    Serial.println("Clear all AP profiles");
+    WiFi.clearProfiles();
+  }
+  
+  // attempt to connect to Wifi network:
+  do {
+    Serial.print("Attempting to connect to ssid: ");
+    Serial.println(ssid);
+  } while( WiFi.begin(ssid, pass) != WL_CONNECTED);
+  
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+  
+  // Save the current AP Profile
+  Serial.print("Saving current connected network to profile list ... ");
+  if ( WiFi.saveProfile() )
+  {
+    Serial.println("Done");
+  }else
+  {
+    Serial.println("Failed");
+  }
+  
+  Serial.print("Checking ");
+  Serial.print(ssid);
+  Serial.print(" in the AP profile list ... ");
+  if ( WiFi.checkProfile(ssid) )
+  {
+    Serial.println("Existed");
+  }else
+  {
+    Serial.println("Not existed");
+  }
+
+  Serial.println("Disconnecting from AP");
+  WiFi.disconnect();
+  
+  Serial.println("Reconnecting with the saved profile");
+  WiFi.begin();
+}
+
+void loop() {
+  // check the network connection once every 10 seconds:
+  delay(10000);
+  printCurrentNet();
+}
+
+void printWifiData() {
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+
+  // print your subnet mask:
+  IPAddress subnet = WiFi.subnetMask();
+  Serial.print("NetMask: ");
+  Serial.println(subnet);
+
+  // print your gateway address:
+  IPAddress gateway = WiFi.gatewayIP();
+  Serial.print("Gateway: ");
+  Serial.println(gateway);
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  Serial.print(bssid[5], HEX);
+  Serial.print(":");
+  Serial.print(bssid[4], HEX);
+  Serial.print(":");
+  Serial.print(bssid[3], HEX);
+  Serial.print(":");
+  Serial.print(bssid[2], HEX);
+  Serial.print(":");
+  Serial.print(bssid[1], HEX);
+  Serial.print(":");
+  Serial.println(bssid[0], HEX);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  int encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
