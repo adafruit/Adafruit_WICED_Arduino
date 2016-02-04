d02760d94e768d3a39b856bfe9c5cb181c71ffcd
hathach <thach@tinyusb.org>
2016-02-04 16:56:53 +0700

    update gpio test

--- examples/Adafruit/GPIO/GpioTest/GpioTest.ino
+++ examples/Adafruit/GPIO/GpioTest/GpioTest.ino
@@ -28,6 +28,48 @@ void setup()
   Serial.println("Gpio Test Example");
 }
 
+void test_message(const char* str, int pin)
+{
+  Serial.printf("Enter to set %s as %s", pinName(pinout), str);
+  Serial.println();
+  getUserInput();
+}
+
+void test_output(int pin)
+{
+  Serial.println("  - Set HIGH (Enter to continue)");
+  digitalWrite(pin, HIGH);
+  getUserInput();
+
+  Serial.println("  - Set LOW (Enter to continue)");
+  digitalWrite(pin, LOW) ;
+  getUserInput();
+}
+
+void test_pwm(int pin)
+{
+  Serial.println("  - PWM from 0 to 65535 (Enter to continue)");
+  while (!Serial.available())
+  {
+    for (int fadeValue = 0; fadeValue <= 65535; fadeValue += 1280) {
+      // Sets the value (range from 0 to 65535):
+      pwmWrite(pin, fadeValue);
+      // Wait for 30 milliseconds to see the dimming effect:
+      delay(30);
+    }
+
+    // Fade out from max to min in increments of 1280 points:
+    for (int fadeValue = 65535 ; fadeValue >= 0; fadeValue -= 1280) {
+      // Sets the value (range from 0 to 1280):
+      pwmWrite(pin, fadeValue);
+      // Wait for 30 milliseconds to see the dimming effect:
+      delay(30);
+    }
+  }
+
+  getUserInput();
+}
+
 // the loop function runs over and over again forever
 void loop() 
 {
@@ -35,68 +77,64 @@ void loop()
   // OUTPUT TEST
   //--------------------------------------------------------------------+
   // OUTPUT Push Pull mode
-  Serial.printf("Send any keys to set %s as OUTPUT PUSH PULL", pinName(pinout));
-  getUserInput();
-  Serial.println();
-
+  test_message("OUTPUT PUSH PULL", pinout);
   pinMode(pinout, OUTPUT);
-
-  digitalWrite(pinout, HIGH); delay(1000);
-  digitalWrite(pinout, LOW) ; delay(1000);
+  test_output(pinout);
 
   // OUTPUT Open Drain mode
-  Serial.printf("Send any keys to set %s as OUTPUT OPEN DRAIN", pinName(pinout));
-  getUserInput();
-  Serial.println();
-
+  test_message("OUTPUT OPENDRAIN", pinout);
   pinMode(pinout, OUTPUT_OPEN_DRAIN);
-
-  digitalWrite(pinout, HIGH); delay(1000);
-  digitalWrite(pinout, LOW) ; delay(1000);
+  test_output(pinout);
 
   //--------------------------------------------------------------------+
   // PWM Test
   //--------------------------------------------------------------------+
   // PWM mode
-  Serial.printf("Send any keys to set %s as PWM", pinName(pinout));
-  getUserInput();
-  Serial.println();
-
+  test_message("PWM", pinout);
   pinMode(pinout, PWM);
+  test_pwm(pinout);
 
   // PWM Opendrain mode
-  Serial.printf("Send any keys to set %s as PWM OPEN DRAIN", pinName(pinout));
-  getUserInput();
-  Serial.println();
-
+  test_message("PWM OPENDRAIN", pinout);
   pinMode(pinout, PWM_OPEN_DRAIN);
+  test_pwm(pinout);
 
   //--------------------------------------------------------------------+
   // INPUT Test
   //--------------------------------------------------------------------+
   // Input Floating (no pull)
-  Serial.printf("Send any keys to set %s as INPUT FLOATING (no pull)", pinName(pinin));
-  getUserInput();
+  Serial.printf("Enter to set %s as INPUT FLOATING (no pull)", pinName(pinin));
   Serial.println();
+  getUserInput();
 
   pinMode(pinin, INPUT);
   /* do something with input pin */
 
   // Input Pullup
-  Serial.printf("Send any keys to set %s as INPUT PULLUP", pinName(pinin));
-  getUserInput();
+  Serial.printf("Enter to set %s as INPUT PULLUP", pinName(pinin));
   Serial.println();
+  getUserInput();
 
-  pinMode(pinin, PULLUP);
+  pinMode(pinin, INPUT_PULLUP);
   /* do something with input pin */
 
   // Input Pulldown
-  Serial.printf("Send any keys to set %s as INPUT PULLDOWN", pinName(pinin));
-  getUserInput();
+  Serial.printf("Enter to set %s as INPUT PULLDOWN", pinName(pinin));
   Serial.println();
+  getUserInput();
 
-  pinMode(pinin, PULLDOWN);
+  pinMode(pinin, INPUT_PULLDOWN);
   /* do something with input pin */
+
+  // Input Analog
+  Serial.printf("Enter to set %s as INPUT ANALOG", pinName(pinin));
+  Serial.println();
+  getUserInput();
+
+  pinMode(pinin, INPUT_ANALOG);
+  /* do something with input pin */
+  
+  Serial.println();
 }
 
 char* getUserInput(void)
@@ -110,8 +148,8 @@ char* getUserInput(void)
   uint8_t count=0;
   do
   {
-    count += Serial.readBytes(inputs+count, maxSize);
-  } while( (count < maxSize) && Serial.available() );
+    count += Serial.readBytes(inputs+count, 64);
+  } while( (count < 64) && Serial.available() );
 
   return inputs;
 }
