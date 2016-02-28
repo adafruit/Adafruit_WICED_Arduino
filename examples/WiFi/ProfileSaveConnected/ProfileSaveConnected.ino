2d5646d2e3a49d6a34be043466f7f7df6fa0817e
hathach <thach@tinyusb.org>
2016-02-28 18:49:33 +0700

    update ProfileSaveConnected.ino

--- examples/WiFi/ProfileSaveConnected/ProfileSaveConnected.ino
+++ examples/WiFi/ProfileSaveConnected/ProfileSaveConnected.ino
@@ -14,35 +14,65 @@
 
 #include <adafruit_feather.h>
 
-char ssid[]   = "yourSSID";     //  your network SSID (name)
-char pass[]   = "yourPassword";  // your network password
+#define WLAN_SSID     "yourSSID"
+#define WLAN_PASS     "yourPassword"
 
-void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    delay(1); // wait for serial port to connect. Needed for native USB port only
+/**************************************************************************/
+/*!
+    @brief  Connect to defined Access Point
+*/
+/**************************************************************************/
+bool connectAP(void)
+{
+  // Attempt to connect to an AP
+  Serial.print("Attempting to connect to: ");
+  Serial.println(WLAN_SSID);
+
+  if ( Feather.connect(WLAN_SSID, WLAN_PASS) )
+  {
+    Serial.println("Connected!");
   }
+  else
+  {
+    Serial.printf("Failed! %s (%d)", Feather.errstr(), Feather.errno());
+    Serial.println();
+  }
+  Serial.println();
 
-  Serial.println("Clear all AP profiles");
+  return Feather.connected();
+}
+
+/**************************************************************************/
+/*!
+    @brief  The setup function runs once when reset the board
+*/
+/**************************************************************************/
+void setup()
+{
+  Serial.begin(115200);
+
+  // wait for serial port to connect. Needed for native USB port only
+  while (!Serial) delay(1);
+
+  Serial.println(F("Profile Save Connected Example\r\n"));
+
+  Serial.print("\r\nClear all AP profiles ... ");
   Feather.clearProfiles();
+  Serial.println("OK");
   
-  // attempt to connect to Wifi network:
-  do {
-    Serial.print("Attempting to connect to ssid: ");
-    Serial.println(ssid);
-  } while( !Feather.connect(ssid, pass) );
+  while ( !connectAP() )
+  {
+    delay(500); // delay between each attempt
+  }
   
-  // you're connected now, so print out the data:
-  Serial.print("You're connected to the network");
-  printCurrentNet();
-  printWifiData();
+  // Connected: Print network info
+  Feather.printNetwork();
   
   // Save the current AP Profile
   Serial.print("Saving current connected network to profile list ... ");
   if ( Feather.saveConnectedProfile() )
   {
-    Serial.println("Done");
+    Serial.println("OK");
   }else
   {
     Serial.println("Failed");
@@ -50,126 +80,42 @@ void setup() {
   
   // Print AP profile list
   Serial.println("Saved AP profile");
-  Serial.println("ID Sec  SSID");
+  Serial.println("ID SSID                 Sec");
   for(uint8_t i=0; i<WIFI_MAX_PROFILE; i++)
   {
     char * profile_ssid = Feather.profileSSID(i);
+    int32_t profile_enc = Feather.profileEncryptionType(i);
 
     Serial.printf("%02d ", i);
     if ( profile_ssid != NULL )
     {
-      printEncryptionType( Feather.profileEncryptionType(i) );
-      Serial.print(" ");
-      Serial.println(profile_ssid);
+      Serial.printf("%-20s ", profile_ssid);
+      Feather.printEncryption( profile_enc );
+      Serial.println();
     }else
     {
       Serial.println("Not Available ");
     }
   }
+  Serial.println();
   
-  Serial.println("Disconnecting from AP");
+  Serial.println("Disconnecting from AP ... ");
   Feather.disconnect();
+  Serial.println("OK");
   
   Serial.println("Reconnecting with the saved profile (should be quicker)");
   Feather.connect(); // no parameters --> using saved profiles
 }
 
+/**************************************************************************/
+/*!
+    @brief  The loop function runs over and over again forever
+*/
+/**************************************************************************/
 void loop() {
-  // check the network connection once every 10 seconds:
+  // Print every 10 seconds
   delay(10000);
-  printCurrentNet();
-}
 
-void printWifiData() {
-  // print your WiFi shield's IP address:
-  IPAddress ip = Feather.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-  // print your MAC address:
-  byte mac[6];
-  Feather.macAddress(mac);
-  Serial.print("MAC address: ");
-  Serial.print(mac[5], HEX);
-  Serial.print(":");
-  Serial.print(mac[4], HEX);
-  Serial.print(":");
-  Serial.print(mac[3], HEX);
-  Serial.print(":");
-  Serial.print(mac[2], HEX);
-  Serial.print(":");
-  Serial.print(mac[1], HEX);
-  Serial.print(":");
-  Serial.println(mac[0], HEX);
-
-  // print your subnet mask:
-  IPAddress subnet = Feather.subnetMask();
-  Serial.print("NetMask: ");
-  Serial.println(subnet);
-
-  // print your gateway address:
-  IPAddress gateway = Feather.gatewayIP();
-  Serial.print("Gateway: ");
-  Serial.println(gateway);
-}
-
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(Feather.SSID());
-
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  Feather.BSSID(bssid);
-  Serial.print("BSSID: ");
-  Serial.print(bssid[5], HEX);
-  Serial.print(":");
-  Serial.print(bssid[4], HEX);
-  Serial.print(":");
-  Serial.print(bssid[3], HEX);
-  Serial.print(":");
-  Serial.print(bssid[2], HEX);
-  Serial.print(":");
-  Serial.print(bssid[1], HEX);
-  Serial.print(":");
-  Serial.println(bssid[0], HEX);
-
-  // print the received signal strength:
-  long rssi = Feather.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-
-  // print the encryption type:
-  Serial.print("Encryption Type: ");
-  printEncryptionType( Feather.encryptionType() );
-  Serial.println();
-}
-
-void printEncryptionType(int32_t thisType) {
-  // read the encryption type and print out the name:
-  switch (thisType) {
-    case ENC_TYPE_WEP:
-    case ENC_TYPE_WEP_SHARED:
-      Serial.print("WEP");
-      break;
-
-    case ENC_TYPE_WPA_TKIP:
-    case ENC_TYPE_WPA_AES:
-      Serial.print("WPA");
-      break;
-
-    case ENC_TYPE_WPA2_AES:
-    case ENC_TYPE_WPA2_TKIP:
-    case ENC_TYPE_WPA2_MIXED:
-      Serial.print("WPA2");
-      break;
-
-    case ENC_TYPE_OPEN:
-      Serial.print("None");
-      break;
-
-//    case ENC_TYPE_AUTO:
-//      Serial.print("Auto");
-//      break;
-  }
+  Feather.printNetwork();
 }
 
