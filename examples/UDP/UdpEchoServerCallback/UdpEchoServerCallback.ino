27a3185d4a582f38a6ad4e8725bb66d6e80fd479
hathach <thach@tinyusb.org>
2016-02-28 21:08:47 +0700

    clean up UdpEchoServerCallback.ino

--- examples/UDP/UdpEchoServerCallback/UdpEchoServerCallback.ino
+++ examples/UDP/UdpEchoServerCallback/UdpEchoServerCallback.ino
@@ -15,67 +15,11 @@
 #include <adafruit_feather.h>
 
 #define WLAN_SSID            "yourSSID"
-#define WLAN_PASS            "yourPass"
-
+#define WLAN_PASS            "yourPassword"
 #define LOCAL_PORT           8888
 
 AdafruitUDP udp;
-
 char packetBuffer[255];
-
-bool connectAP(void)
-{
-  // Attempt to connect to an AP
-  Serial.print("Attempting to connect to: ");
-  Serial.println(WLAN_SSID);
-
-  if ( Feather.connect(WLAN_SSID, WLAN_PASS) )
-  {
-    Serial.println("Connected!");
-  }
-  else
-  {
-    Serial.printf("Failed! %s (%d)", Feather.errstr(), Feather.errno());
-    Serial.println();
-  }
-  Serial.println();
-
-  return Feather.connected();
-}
-
-void setup()
-{
-  Serial.begin(115200);
-
-  // wait for Serial port to connect. Needed for native USB port only
-  while (!Serial) delay(1);
-
-  Serial.println("UDP Echo Server Callback Example");
-  Serial.println();
-
-  while( !connectAP() )
-  {
-    delay(500);
-  }
-
-  // Tell the UDP client to auto print error codes and halt on errors
-  udp.err_actions(true, true);
-  udp.setReceivedCallback(received_callback);
-
-  Serial.printf("Openning UDP at port %d ... ", LOCAL_PORT);
-  udp.begin(LOCAL_PORT);
-  Serial.println("OK");
-
-  Serial.println("Please use your PC/mobile and send any text to ");
-  Serial.print( IPAddress(Feather.localIP()) );
-  Serial.print(" UDP port ");
-  Serial.println(LOCAL_PORT);
-}
-
-void loop()
-{
-
-}
 
 /**************************************************************************/
 /*!
@@ -104,4 +48,79 @@ void received_callback(void)
     udp.write(packetBuffer, packetSize);
     udp.endPacket();
   }
+}
+
+/**************************************************************************/
+/*!
+    @brief  The setup function runs once when reset the board
+*/
+/**************************************************************************/
+void setup()
+{
+  Serial.begin(115200);
+
+  // wait for serial port to connect. Needed for native USB port only
+  while (!Serial) delay(1);
+
+  Serial.println("UDP Echo Server Callback Example\r\n");
+
+  // Print all software verions
+  Feather.printVersions();
+
+  while ( !connectAP() )
+  {
+    delay(500); // delay between each attempt
+  }
+
+  // Connected: Print network info
+  Feather.printNetwork();
+
+
+  // Tell the UDP client to auto print error codes and halt on errors
+  udp.err_actions(true, true);
+  udp.setReceivedCallback(received_callback);
+
+  Serial.printf("Openning UDP at port %d ... ", LOCAL_PORT);
+  udp.begin(LOCAL_PORT);
+  Serial.println("OK");
+
+  Serial.println("Please use your PC/mobile and send any text to ");
+  Serial.print( IPAddress(Feather.localIP()) );
+  Serial.print(" UDP port ");
+  Serial.println(LOCAL_PORT);
+}
+
+/**************************************************************************/
+/*!
+    @brief  The loop function runs over and over again forever
+*/
+/**************************************************************************/
+void loop()
+{
+
+}
+
+/**************************************************************************/
+/*!
+    @brief  Connect to defined Access Point
+*/
+/**************************************************************************/
+bool connectAP(void)
+{
+  // Attempt to connect to an AP
+  Serial.print("Attempting to connect to: ");
+  Serial.println(WLAN_SSID);
+
+  if ( Feather.connect(WLAN_SSID, WLAN_PASS) )
+  {
+    Serial.println("Connected!");
+  }
+  else
+  {
+    Serial.printf("Failed! %s (%d)", Feather.errstr(), Feather.errno());
+    Serial.println();
+  }
+  Serial.println();
+
+  return Feather.connected();
 }
