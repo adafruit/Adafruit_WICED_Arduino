1a0a02da911b28fb3069c1d13ae2c8e895caf97a
hathach <thach@tinyusb.org>
2016-03-01 20:41:42 +0700

    added AIO demo

---
+++ examples/AIO/AIOTest/AIOTest.ino
@@ -0,0 +1,126 @@
+/*********************************************************************
+ This is an example for our Feather WIFI modules
+
+ Pick one up today in the adafruit shop!
+
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+#include <adafruit_feather.h>
+#include <adafruit_mqtt.h>
+#include <adafruit_aio.h>
+
+/* This sketch connect to the Adafruit IO sever io.adafruit.com
+ * and update a defined feed every 1 second.
+ *
+ * To run this demo
+ * 1. Change SSID/PASS
+ * 2. Decide whether you want to use TLS/SSL or not (USE_TLS)
+ * 3. Change CLIENTID, TOPIC, PUBLISH_MESSAGE, WILL_MESSAGE if you want
+ * 4. Compile and run
+ * 5. Use MQTT desktop client to connect to same sever and subscribe to the defined topic
+ * to monitor the published message.
+ */
+
+#define WLAN_SSID         "yourSSID"
+#define WLAN_PASS         "yourPass"
+
+#define AIO_USERNAME      "...your AIO username (see https://accounts.adafruit.com)..."
+#define AIO_KEY           "...your AIO key..."
+
+// AdafruitAIOwill auto append "username/feeds/" prefix to your feed
+#define AIO_FEED          "photocell"
+
+#define USE_TLS           1
+#define CLIENTID          "Adafruit Feather"
+
+AdafruitAIO     aio(AIO_USERNAME, AIO_KEY, CLIENTID);
+AdafruitAIOFeed feed(&aio, AIO_FEED);
+
+int value = 0;
+
+/**************************************************************************/
+/*!
+    @brief  The setup function runs once when the board comes out of reset
+*/
+/**************************************************************************/
+void setup()
+{
+  Serial.begin(115200);
+
+  // Wait for the USB serial port to connect. Needed for native USB port only
+  while (!Serial) delay(1);
+
+  Serial.println("AIO Test Example\r\n");
+
+  // Print all software versions
+  Feather.printVersions();
+
+  while ( !connectAP() )
+  {
+    delay(500); // delay between each attempt
+  }
+
+  // Connected: Print network info
+  Feather.printNetwork();
+
+  // Tell the MQTT client to auto print error codes and halt on errors
+  aio.err_actions(true, true);
+
+  Serial.print("Connecting to io.adafruit.com ... ");
+  if ( USE_TLS )
+  {
+    aio.connectSSL(); // Will halted if an error occurs
+  }else
+  {
+    aio.connect(); // Will halted if an error occurs
+  }
+  Serial.println("OK");
+}
+
+/**************************************************************************/
+/*!
+    @brief  This loop function runs over and over again
+*/
+/**************************************************************************/
+void loop()
+{
+  value = (value+1) % 100;
+
+  Serial.print("Updating feed " AIO_FEED " ... ");
+  feed.print(value);
+  Serial.println("OK");
+
+  delay(1000);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Connect to defined Access Point
+*/
+/**************************************************************************/
+bool connectAP(void)
+{
+  // Attempt to connect to an AP
+  Serial.print("Attempting to connect to: ");
+  Serial.println(WLAN_SSID);
+
+  if ( Feather.connect(WLAN_SSID, WLAN_PASS) )
+  {
+    Serial.println("Connected!");
+  }
+  else
+  {
+    Serial.printf("Failed! %s (%d)", Feather.errstr(), Feather.errno());
+    Serial.println();
+  }
+  Serial.println();
+
+  return Feather.connected();
+}
