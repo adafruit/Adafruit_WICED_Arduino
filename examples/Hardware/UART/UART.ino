056ef8cfbc9a73a74265f2229a4f06cb39fa2bec
hathach <thach@tinyusb.org>
2016-03-14 19:27:07 +0700

    UART description

--- examples/Hardware/UART/UART.ino
+++ examples/Hardware/UART/UART.ino
@@ -1,52 +1,40 @@
-/**************************************************************************/
-/*!
-    @file     UART
-    @author   hathach
+/*********************************************************************
+ This is an example for our Feather WIFI modules
 
-    @section LICENSE
+ Pick one up today in the adafruit shop!
 
-    Software License Agreement (BSD License)
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
 
-    Copyright (c) 2015, Adafruit Industries (adafruit.com)
-    All rights reserved.
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
 
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions are met:
-    1. Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holders nor the
-    names of its contributors may be used to endorse or promote products
-    derived from this software without specific prior written permission.
+/* This example demonstrate that USBSerial and all HW UARTs (1,2,4) are working.
+ * Sketch will echo any characters from any Serials to all the Serials.
+ */
 
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
-    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
-    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-/**************************************************************************/
 #include <Arduino.h>
 
-void setup() {
+/**************************************************************************/
+/*!
+    @brief  The setup function runs once when reset the board
+*/
+/**************************************************************************/
+void setup()
+{
   Serial.begin (115200);  // USB monitor
   Serial1.begin(115200);  // HW UART1
   Serial2.begin(115200);  // HW UART2
-  //Serial3.begin(115200);  // HW UART4 TODO not working yet
   
+  // wait for the serial port to connect. Needed for native USB port only.
   while (!Serial) delay(1);
+
   Serial.println ("UART demo: Serial Monitor");
   Serial1.println("UART demo: HWUART1");
   Serial2.println("UART demo: HWUART2");
-  
-  //Serial3.println("UART demo: HWUART4");
 }
 
 void printAll(char ch)
@@ -54,10 +42,15 @@ void printAll(char ch)
   Serial.print (ch);
   Serial1.print(ch);
   Serial2.print(ch);
-  //Serial3.print(ch);
 }
 
-void loop() {
+/**************************************************************************/
+/*!
+    @brief  The loop function runs over and over again forever
+*/
+/**************************************************************************/
+void loop()
+{
   char ch;
   
   // From Serial monitor to All
