e2c616511be4de5a9c4b0f35db736f1a76e6d238
hathach <thach@tinyusb.org>
2016-06-30 17:38:34 +0700

    clean up

--- examples/Email/SendMail/SendMail.ino
+++ examples/Email/SendMail/SendMail.ino
@@ -64,7 +64,7 @@ void setup()
   Serial.begin(115200);
 
   // wait for serial port to connect. Needed for native USB port only
-  //while (!Serial) delay(1);
+  while (!Serial) delay(1);
 
   Serial.println("Send Mail Example\r\n");
 
