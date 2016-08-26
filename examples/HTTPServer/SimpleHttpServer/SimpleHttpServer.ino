d4c40c5514c433a08e364f8f2cbf3098d7629291
hathach <thach@tinyusb.org>
2016-08-26 17:05:35 +0700

    add SimpleHttpServer.ino

---
+++ examples/HTTPServer/SimpleHttpServer/SimpleHttpServer.ino
@@ -0,0 +1,125 @@
+/*********************************************************************
+ This is an example for our WICED Feather WIFI modules
+
+ Pick one up today in the adafruit shop!
+
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+/* This example use the AdafruitHTTP class to send requests and get
+ * the response via a callback */
+
+#include <adafruit_feather.h>
+#include <adafruit_http_server.h>
+
+#define WLAN_SSID            "yourSSID"
+#define WLAN_PASS            "yourPassword"
+
+#define PORT                 80                     // The TCP port to use
+
+int ledPin = PA15;
+
+// Use the HTTP class
+AdafruitHTTPServer httpserver(5);
+
+const char hello_html[] = "<h2>Hello World<h2>";
+
+HTTPPage pages[] = 
+{
+  HTTPPage("/info.html", HTTP_MIME_TEXT_PLAIN, hello_html),
+  //HTTPPage("/hello.html", HTTP_MIME_TEXT_HTML, info_html),
+};
+
+
+/**************************************************************************/
+/*!
+    @brief  TCP/HTTP disconnect callback
+*/
+/**************************************************************************/
+void disconnect_callback(void)
+{
+  Serial.println();
+  Serial.println("---------------------");
+  Serial.println("DISCONNECTED CALLBACK");
+  Serial.println("---------------------");
+  Serial.println();
+
+  httpserver.stop();
+}
+
+/**************************************************************************/
+/*!
+    @brief  The setup function runs once when the board comes out of reset
+*/
+/**************************************************************************/
+void setup()
+{
+  Serial.begin(115200);
+
+  // Wait for the USB serial to connect. Needed for native USB port only.
+  while (!Serial) delay(1);
+
+  Serial.println("Simple HTTP Server Example\r\n");
+  
+  // Print all software versions
+  Feather.printVersions();
+
+  // Try to connect to an AP
+  while ( !connectAP() )
+  {
+    delay(500); // delay between each attempt
+  }
+
+  // Connected: Print network info
+  Feather.printNetwork();
+
+  // Tell the HTTP client to auto print error codes and halt on errors
+  httpserver.err_actions(true, true);
+
+  // Configure HTTP Server Pages
+  Serial.println("Adding Pages to HTTP Server");
+  httpserver.addPages(pages, 1);
+
+  httpserver.begin(80, 3);
+}
+
+/**************************************************************************/
+/*!
+    @brief  The loop function runs over and over again
+*/
+/**************************************************************************/
+void loop()
+{
+  togglePin(ledPin);
+  delay(1000);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Connect to the defined access point (AP)
+*/
+/**************************************************************************/
+bool connectAP(void)
+{
+  // Attempt to connect to an AP
+  Serial.print("Please wait while connecting to: '" WLAN_SSID "' ... ");
+
+  if ( Feather.connect(WLAN_SSID, WLAN_PASS) )
+  {
+    Serial.println("Connected!");
+  }
+  else
+  {
+    Serial.printf("Failed! %s (%d)", Feather.errstr(), Feather.errno());
+    Serial.println();
+  }
+  Serial.println();
+
+  return Feather.connected();
+}
