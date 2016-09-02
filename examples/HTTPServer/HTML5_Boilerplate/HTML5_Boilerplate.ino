d198dc0ba4c7f9aedab6176b573344b181ee6480
microbuilder <contact@microbuilder.eu>
2016-09-02 10:18:22 +0200

    Updated the sample dynamic page to user boilerplate

--- examples/HTTPServer/HTML5_Boilerplate/HTML5_Boilerplate.ino
+++ examples/HTTPServer/HTML5_Boilerplate/HTML5_Boilerplate.ino
@@ -12,25 +12,25 @@
  any redistribution
 *********************************************************************/
 
-/* This example use HTML Boilerplate 5.3.0 on the HTTP server to illustrate 
+/* This example use HTML Boilerplate 5.3.0 on the HTTP server to illustrate
  *  how to create a flexible web site on your WICED Feather.
- *  
+ *
  * The raw source files used in this demo are included in resources folder.
- * 
+ *
  * To convert the source files (html, jpg etc..) into C headers, please use
  * the included 'tools/pyresource' script:
  *
  *   $ python ../../../tools/pyresource/pyresource.py resources
- * 
+ *
  * pyresource will convert each file to a header file, and '.' will be
  * replaced by '_' in the header file name(s). For example:
- * 
+ *
  *   index.html --> index_html.h with HTTPResource variable name 'index_html'
- *    
- * For your convenience, a master header named resources.h is also generated 
+ *
+ * For your convenience, a master header named resources.h is also generated
  * which includes all of generated header files. Your sketch simply needs to
  * include a reference to this file:
- * 
+ *
  *   #include "resources.h"
  */
 
@@ -38,42 +38,41 @@
 #include <adafruit_http_server.h>
 #include "resources.h"
 
-#define WLAN_SSID            "yourSSID"
-#define WLAN_PASS            "yourPassword"
+#define WLAN_SSID            "your_ssid"
+#define WLAN_PASS            "your_password"
 
 #define PORT                 80            // The TCP port to use
 #define MAX_CLIENTS          3
 
 int ledPin = PA15;
 
-void heap_generator (const char* url, const char* query, void* http_request);
-void thread_generator (const char* url, const char* query, void* http_request);
+void about_generator (const char* url, const char* query, void* http_request);
 
 // Page database
-HTTPPage pages[] = 
+HTTPPage pages[] =
 {
-  HTTPPageRedirect("/", "/index.html"), // Redirect root requests to '/index.html'
+  // Page redirections
+  HTTPPageRedirect("/", "/index.html"), // Redirect root to '/index.html'
 
-  HTTPPage("/404.html"              , HTTP_MIME_TEXT_HTML           , &err404_html), // Note: files can't start with a number!
-  HTTPPage("/apple-touch-icon.png"  , HTTP_MIME_IMAGE_PNG           , &apple_touch_icon_png),
-  HTTPPage("/browserconfig.xml"     , HTTP_MIME_TEXT_PLAIN          , &browserconfig_xml),
-  HTTPPage("/crossdomain.xml"       , HTTP_MIME_TEXT_PLAIN          , &crossdomain_xml),
-  HTTPPage("/favicon.ico"           , HTTP_MIME_IMAGE_MICROSOFT     , &favicon_ico),
-  HTTPPage("/index.html"            , HTTP_MIME_TEXT_HTML           , &index_html),
-  HTTPPage("/robots.txt"            , HTTP_MIME_TEXT_PLAIN          , &robots_txt),
-  HTTPPage("/tile-wide.png"         , HTTP_MIME_IMAGE_PNG           , &tile_wide_png),
-  HTTPPage("/tile.png"              , HTTP_MIME_IMAGE_PNG           , &tile_png),
+  // Static content
+  HTTPPage("/404.html"                        , HTTP_MIME_TEXT_HTML       , &err404_html), // Note: files can't start with a number!
+  HTTPPage("/apple-touch-icon.png"            , HTTP_MIME_IMAGE_PNG       , &apple_touch_icon_png),
+  HTTPPage("/browserconfig.xml"               , HTTP_MIME_TEXT_PLAIN      , &browserconfig_xml),
+  HTTPPage("/crossdomain.xml"                 , HTTP_MIME_TEXT_PLAIN      , &crossdomain_xml),
+  HTTPPage("/favicon.ico"                     , HTTP_MIME_IMAGE_MICROSOFT , &favicon_ico),
+  HTTPPage("/index.html"                      , HTTP_MIME_TEXT_HTML       , &index_html),
+  HTTPPage("/robots.txt"                      , HTTP_MIME_TEXT_PLAIN      , &robots_txt),
+  HTTPPage("/tile-wide.png"                   , HTTP_MIME_IMAGE_PNG       , &tile_wide_png),
+  HTTPPage("/tile.png"                        , HTTP_MIME_IMAGE_PNG       , &tile_png),
+  HTTPPage("/css/main.css"                    , HTTP_MIME_TEXT_CSS        , &main_css),
+  HTTPPage("/css/normalize.css"               , HTTP_MIME_TEXT_CSS        , &normalize_css),
+  HTTPPage("/js/main.js"                      , HTTP_MIME_JAVASCRIPT      , &main_js),
+  HTTPPage("/js/plugins.js"                   , HTTP_MIME_JAVASCRIPT      , &plugins_js),
+  HTTPPage("/js/vendor/jquery-1.12.0.min.js"  , HTTP_MIME_JAVASCRIPT      , &jquery_1_12_0_min_js),
+  HTTPPage("/js/vendor/modernizr-2.8.3.min.js", HTTP_MIME_JAVASCRIPT      , &modernizr_2_8_3_min_js),
 
-  HTTPPage("/css/main.css"          , HTTP_MIME_TEXT_CSS            , &main_css),
-  HTTPPage("/css/normalize.css"     , HTTP_MIME_TEXT_CSS            , &normalize_css),
-
-  HTTPPage("/js/main.js"            , HTTP_MIME_JAVASCRIPT          , &main_js),
-  HTTPPage("/js/plugins.js"         , HTTP_MIME_JAVASCRIPT          , &plugins_js),
-  HTTPPage("/js/vendor/jquery-1.12.0.min.js", HTTP_MIME_JAVASCRIPT  , &jquery_1_12_0_min_js),
-  HTTPPage("/js/vendor/modernizr-2.8.3.min.js", HTTP_MIME_JAVASCRIPT, &modernizr_2_8_3_min_js),
-
-  HTTPPage("/heap.csv"   , HTTP_MIME_TEXT_PLAIN, heap_generator),
-  HTTPPage("/thread.csv" , HTTP_MIME_TEXT_PLAIN, thread_generator),
+  // Dynamic content
+  HTTPPage("/about.html", HTTP_MIME_TEXT_HTML, about_generator),
 };
 
 uint8_t pagecount = sizeof(pages)/sizeof(HTTPPage);
@@ -83,40 +82,75 @@ AdafruitHTTPServer httpserver(pagecount);
 
 thread_info_t threadInfo[20];
 
-// This dynamic page generate populates a CSV file with heap memory usage details
-void heap_generator (const char* url, const char* query, void* http_request)
+uint32_t visit_count = 0;
+
+/**************************************************************************/
+/*!
+    @brief  Example of generating dynamic HTML content on demand
+*/
+/**************************************************************************/
+void about_generator (const char* url, const char* query, void* http_request)
 {
   (void) url;
   (void) query;
   (void) http_request;
 
-  int used_size = Feather.dbgHeapUsed();
-  int free_size = Feather.dbgHeapFree();
+  visit_count++;
 
-  httpserver.print("label,count\r\n");
-  httpserver.printf("Used %ld,%ld\r\n", used_size, used_size);
-  httpserver.printf("Free %ld,%ld\r\n", free_size, free_size);
-}
-
-// This dynamic page generate populates a CSV file with info on FeatherLib RTOS threads
-void thread_generator (const char* url, const char* query, void* http_request)
-{
-  (void) url;
-  (void) query;
-  (void) http_request;
-
-  httpserver.print("Name,Used,Free\r\n");
+  // Note that all double quotes have been replaced with single quotes in
+  // the HTML code below, taken from the default index.html template file.
+  // Some double quotes and backslashes have also been escaped ('\' to '\\',
+  // '"' to '\"') where the character needing an escape code couldn't be
+  // avoided.
   
-  // Get Thread Info
-  int count = Feather.dbgThreadInfo(threadInfo, 20);
-  //DBG_INT(count);
+  httpserver.print("<!doctype html>");
+  httpserver.print("<html class='no-js' lang=''>");
+  httpserver.print("    <head>");
+  httpserver.print("        <meta charset='utf-8'>");
+  httpserver.print("        <meta http-equiv='x-ua-compatible' content='ie=edge'>");
+  httpserver.print("        <title></title>");
+  httpserver.print("        <meta name='description' content=''>");
+  httpserver.print("        <meta name='viewport' content='width=device-width, initial-scale=1'>");
+  httpserver.print("        <link rel='apple-touch-icon' href='apple-touch-icon.png'>");
+  httpserver.print("        <link rel='stylesheet' href='css/normalize.css'>");
+  httpserver.print("        <link rel='stylesheet' href='css/main.css'>");
+  httpserver.print("        <script src='js/vendor/modernizr-2.8.3.min.js'></script>");
+  httpserver.print("    </head>");
+  httpserver.print("    <body>");
+  httpserver.print("        <!--[if lt IE 8]>");
+  httpserver.print("            <p class='browserupgrade'>You are using an <strong>outdated</strong> browser. Please <a href='http://browsehappy.com/'>upgrade your browser</a> to improve your experience.</p>");
+  httpserver.print("        <![endif]-->");
   
-  for(int i=0; i<count; i++)
-  {
-    httpserver.printf("%s (%u),%ld,%ld\r\n", 
-                      threadInfo[i].name, threadInfo[i].prio,
-                      threadInfo[i].stack_highest, threadInfo[i].stack_total-threadInfo[i].stack_highest);
-  }
+  // Insert the dynamic body content here
+  httpserver.print("        <h2>Module Details</h2>");
+  httpserver.print("        <ul>");
+  httpserver.print("            <li><b>Bootloader</b> : "); httpserver.print( Feather.bootloaderVersion() ); httpserver.print("</li>");
+  httpserver.print("            <li><b>WICED SDK</b> : "); httpserver.print( Feather.sdkVersion() ); httpserver.print("</li>");
+  httpserver.print("            <li><b>FeatherLib</b> : "); httpserver.print( Feather.firmwareVersion() ); httpserver.print("</li>");
+  httpserver.print("            <li><b>Arduino API</b> : "); httpserver.print( Feather.arduinoVersion() ); httpserver.print("</li>");
+  httpserver.print("        </ul>");
+  httpserver.print("        <h2>Page Details</h2>");
+  httpserver.print("        <ul>");
+  httpserver.print("            <li><b>Page Loads</b> : "); httpserver.print( visit_count ); httpserver.print("</li>");
+  httpserver.print("        </ul>");
+  httpserver.print("        <p>This page has been dynamically generated.</p>");
+  
+  // Take note of the escaped backslash and double-quotes in the text below
+  httpserver.print("        <script src='https://code.jquery.com/jquery-1.12.0.min.js'></script>");
+  httpserver.print("        <script>window.jQuery || document.write('<script src=\"js/vendor/jquery-1.12.0.min.js\"><\\/script>')</script>");
+  httpserver.print("        <script src='js/plugins.js'></script>");
+  httpserver.print("        <script src='js/main.js'></script>");
+  httpserver.print("        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->");
+  httpserver.print("        <script>");
+  httpserver.print("            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=");
+  httpserver.print("            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;");
+  httpserver.print("            e=o.createElement(i);r=o.getElementsByTagName(i)[0];");
+  httpserver.print("            e.src='https://www.google-analytics.com/analytics.js';");
+  httpserver.print("            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));");
+  httpserver.print("            ga('create','UA-XXXXX-X','auto');ga('send','pageview');");
+  httpserver.print("        </script>");
+  httpserver.print("    </body>");
+  httpserver.print("</html>");
 }
 
 /**************************************************************************/
@@ -132,7 +166,7 @@ void setup()
   while (!Serial) delay(1);
 
   Serial.println("Simple HTTP Server Example\r\n");
-  
+
   // Print all software versions
   Feather.printVersions();
 
@@ -207,3 +241,4 @@ void disconnect_callback(void)
 
   httpserver.stop();
 }
+
