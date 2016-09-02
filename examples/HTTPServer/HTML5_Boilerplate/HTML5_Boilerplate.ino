68f3fe7a938b910d20aae804df57e4927d88aa38
microbuilder <contact@microbuilder.eu>
2016-09-02 10:48:47 +0200

    More cleanup on dynamic page generation code

--- examples/HTTPServer/HTML5_Boilerplate/HTML5_Boilerplate.ino
+++ examples/HTTPServer/HTML5_Boilerplate/HTML5_Boilerplate.ino
@@ -102,55 +102,55 @@ void about_generator (const char* url, const char* query, void* http_request)
   // Some double quotes and backslashes have also been escaped ('\' to '\\',
   // '"' to '\"') where the character needing an escape code couldn't be
   // avoided.
-  
-  httpserver.print("<!doctype html>");
-  httpserver.print("<html class='no-js' lang=''>");
-  httpserver.print("    <head>");
-  httpserver.print("        <meta charset='utf-8'>");
-  httpserver.print("        <meta http-equiv='x-ua-compatible' content='ie=edge'>");
-  httpserver.print("        <title></title>");
-  httpserver.print("        <meta name='description' content=''>");
-  httpserver.print("        <meta name='viewport' content='width=device-width, initial-scale=1'>");
-  httpserver.print("        <link rel='apple-touch-icon' href='apple-touch-icon.png'>");
-  httpserver.print("        <link rel='stylesheet' href='css/normalize.css'>");
-  httpserver.print("        <link rel='stylesheet' href='css/main.css'>");
-  httpserver.print("        <script src='js/vendor/modernizr-2.8.3.min.js'></script>");
-  httpserver.print("    </head>");
-  httpserver.print("    <body>");
-  httpserver.print("        <!--[if lt IE 8]>");
-  httpserver.print("            <p class='browserupgrade'>You are using an <strong>outdated</strong> browser. Please <a href='http://browsehappy.com/'>upgrade your browser</a> to improve your experience.</p>");
-  httpserver.print("        <![endif]-->");
+
+  httpserver.print("<!doctype html>\n"
+                   "<html class='no-js' lang=''>\n"
+                   "    <head>\n"
+                   "        <meta charset='utf-8'>\n"
+                   "        <meta http-equiv='x-ua-compatible' content='ie=edge'>\n"
+                   "        <title></title>\n"
+                   "        <meta name='description' content=''>\n"
+                   "        <meta name='viewport' content='width=device-width, initial-scale=1'>\n"
+                   "        <link rel='apple-touch-icon' href='apple-touch-icon.png'>\n"
+                   "        <link rel='stylesheet' href='css/normalize.css'>\n"
+                   "        <link rel='stylesheet' href='css/main.css'>\n"
+                   "        <script src='js/vendor/modernizr-2.8.3.min.js'></script>\n"
+                   "    </head>\n"
+                   "    <body>\n"
+                   "        <!--[if lt IE 8]>\n"
+                   "            <p class='browserupgrade'>You are using an <strong>outdated</strong> browser. Please <a href='http://browsehappy.com/'>upgrade your browser</a> to improve your experience.</p>\n"
+                   "        <![endif]-->\n");
   
   // Insert the dynamic body content here
-  httpserver.print("        <h2>Module Details</h2>");
-  httpserver.print("        <ul>");
-  httpserver.print("            <li><b>Bootloader</b> : "); httpserver.print( Feather.bootloaderVersion() ); httpserver.print("</li>");
-  httpserver.print("            <li><b>WICED SDK</b> : "); httpserver.print( Feather.sdkVersion() ); httpserver.print("</li>");
-  httpserver.print("            <li><b>FeatherLib</b> : "); httpserver.print( Feather.firmwareVersion() ); httpserver.print("</li>");
-  httpserver.print("            <li><b>Arduino API</b> : "); httpserver.print( Feather.arduinoVersion() ); httpserver.print("</li>");
-  httpserver.print("        </ul>");
-  httpserver.print("        <h2>Page Details</h2>");
-  httpserver.print("        <ul>");
-  httpserver.print("            <li><b>Page Loads</b> : "); httpserver.print( visit_count ); httpserver.print("</li>");
-  httpserver.print("        </ul>");
-  httpserver.print("        <p>This page has been dynamically generated.</p>");
+  httpserver.print("        <h2>Module Details</h2>\n");
+  httpserver.print("        <ul>\n");
+  httpserver.print("            <li><b>Bootloader</b> : "); httpserver.print( Feather.bootloaderVersion() ); httpserver.print("</li>\n");
+  httpserver.print("            <li><b>WICED SDK</b> : "); httpserver.print( Feather.sdkVersion() ); httpserver.print("</li>\n");
+  httpserver.print("            <li><b>FeatherLib</b> : "); httpserver.print( Feather.firmwareVersion() ); httpserver.print("</li>\n");
+  httpserver.print("            <li><b>Arduino API</b> : "); httpserver.print( Feather.arduinoVersion() ); httpserver.print("</li>\n");
+  httpserver.print("        </ul>\n");
+  httpserver.print("        <h2>Page Details</h2>\n");
+  httpserver.print("        <ul>\n");
+  httpserver.print("            <li><b>Page Loads</b> : "); httpserver.print( visit_count ); httpserver.print("</li>\n");
+  httpserver.print("        </ul>\n");
+  httpserver.print("        <p>This page has been dynamically generated.</p>\n");
   
   // Take note of the escaped backslash and double-quotes in the text below
-  httpserver.print("        <script src='https://code.jquery.com/jquery-1.12.0.min.js'></script>");
-  httpserver.print("        <script>window.jQuery || document.write('<script src=\"js/vendor/jquery-1.12.0.min.js\"><\\/script>')</script>");
-  httpserver.print("        <script src='js/plugins.js'></script>");
-  httpserver.print("        <script src='js/main.js'></script>");
-  httpserver.print("        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->");
-  httpserver.print("        <script>");
-  httpserver.print("            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=");
-  httpserver.print("            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;");
-  httpserver.print("            e=o.createElement(i);r=o.getElementsByTagName(i)[0];");
-  httpserver.print("            e.src='https://www.google-analytics.com/analytics.js';");
-  httpserver.print("            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));");
-  httpserver.print("            ga('create','UA-XXXXX-X','auto');ga('send','pageview');");
-  httpserver.print("        </script>");
-  httpserver.print("    </body>");
-  httpserver.print("</html>");
+  httpserver.print("        <script src='https://code.jquery.com/jquery-1.12.0.min.js'></script>\n"
+                   "        <script>window.jQuery || document.write('<script src=\"js/vendor/jquery-1.12.0.min.js\"><\\/script>')</script>\n"
+                   "        <script src='js/plugins.js'></script>\n"
+                   "        <script src='js/main.js'></script>\n"
+                   "        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->\n"
+                   "        <script>\n"
+                   "            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=\n"
+                   "            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;\n"
+                   "            e=o.createElement(i);r=o.getElementsByTagName(i)[0];\n"
+                   "            e.src='https://www.google-analytics.com/analytics.js';\n"
+                   "            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));\n"
+                   "            ga('create','UA-XXXXX-X','auto');ga('send','pageview');\n"
+                   "        </script>\n"
+                   "    </body>\n"
+                   "</html>\n");
 }
 
 /**************************************************************************/
