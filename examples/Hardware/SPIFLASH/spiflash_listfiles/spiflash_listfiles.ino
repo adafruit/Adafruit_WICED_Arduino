d0ec42d6ea4a852c88cd0ffc5db259d96272b43f
hathach <thach@tinyusb.org>
2016-09-07 14:23:57 +0700

    finish list file examples

--- examples/Hardware/SPIFLASH/spiflash_listfiles/spiflash_listfiles.ino
+++ examples/Hardware/SPIFLASH/spiflash_listfiles/spiflash_listfiles.ino
@@ -16,8 +16,14 @@
 #include "adafruit_spiflash.h"
 
 /* This example use SpiFlash print out the SPI Flash's content up to
- * 4 level of directories (including root)
+ * MAX_LEVEL level of directories (including root)
  */
+
+/*
+ * WARNING: This example uses recursive call to print out directory tree
+ * be extra careful, high number of MAX_LEVEL can cause memory overflow
+ */
+#define MAX_LEVEL   2
 
 // the setup function runs once when you press reset or power the board
 void setup() 
@@ -31,28 +37,71 @@ void setup()
   
   SpiFlash.begin();
 
-  // Open root directory
-  FatDir root_dir = SpiFlash.openDir("/");
+  // Print whole directory tree of root whose level is 0
+  printTreeDir("/", 0);
+}
 
+void printTreeDir(const char* path, uint8_t level)
+{
+  // Open the input folder
+  FatDir dir = SpiFlash.openDir(path);
+  
+  // Print root
+  if (level == 0)
+  {
+    Serial.println("root");
+  }else
+  {
+    // Change current directory to this folder to be able to open 
+    // its sub folder with only folder name (relative path)
+    SpiFlash.cd( path );   
+  }
+  
   // File Entry Information which hold file attribute and name
   FatFileInfo finfo;
 
-  // Print root
-  Serial.println('/');
-
-  // Loop through the directory
-  while( root_dir.read(&finfo) )
+  // Loop through the directory  
+  while( dir.read(&finfo) )
   {
-    Serial.print("  ");
+    // Indentation according to dir level
+    for(int i=0; i<level; i++) 
+    {
+      Serial.print("|  ");
+    }
+    Serial.print("|_ ");
     Serial.print( finfo.name() );
+
     if ( finfo.isDirectory() ) 
     {
-      Serial.print("/");
-      
-      FatDir level2_dir = SpiFlash.openDir("/");
+      Serial.println("/");
+
+      // ATTENTION recursive call to print sub folder with level+1 !!!!!!!!
+      // High number of MAX_LEVEL can cause memory overflow
+      if ( level < MAX_LEVEL )
+      {
+        printTreeDir( finfo.name(), level+1 );
+      }
+    }else
+    {
+      // Print file size starting from position 50
+      int pos = level*3 + 3 + strlen(finfo.name());
+
+      // Print at least one space in case current position > 50
+      Serial.print(' ');
+
+      // Print padding
+      for (int i=pos; i<50; i++) Serial.print(' ');
+
+      // Print size in KB
+      Serial.print( finfo.size() / 1024 );
+      Serial.println( " KB");
     }
-    
-    Serial.println();
+  }
+
+  // Change back to Parent if not root
+  if (level != 0)
+  {
+    SpiFlash.cd( ".." );    
   }
 }
 
